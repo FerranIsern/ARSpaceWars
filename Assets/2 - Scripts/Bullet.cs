2433a88a1a91f87d07e469b4b59f997bbdef9193
Ferran Isern Blasco <ferran@isern.blasco.cat>
2018-10-09 19:51:10 +0200

    first commit

---
+++ Assets/2 - Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public GameObject explosion;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(explosion, transform.position, explosion.transform.rotation);
+        Destroy(transform.gameObject);
+    }
+}
